a96be08103cffca037f0447a741c4614f16b47dc
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-17 16:01:33 +0700

    add seeder and migration

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menu>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $menu_name = $this->faker->unique()->words($nb = 5, $asText = true);
+        $slug = Str::slug($menu_name, '-');
+        return [
+            'name' => $menu_name,
+            'slug' => $slug,
+            'description' => $this->faker->text(200),
+            'regular_price' => $this->faker->numberBetween(10, 500),
+            'quantity' => $this->faker->numberBetween(10, 50),
+            'image' => 'menu-' . $this->faker->numberBetween(1, 13),
+        ];
+    }
+}
