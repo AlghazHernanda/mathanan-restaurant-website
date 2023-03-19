5b43a5391edd0bdebb020abfc283d8f370cbcf3b
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-19 16:33:05 +0700

    add cart link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -351,7 +351,7 @@
                                         </ul>
                                     </li>
                                     <li><a href="blog.html">Blog </a></li>                                    
-                                    <li><a href="contact.html">Contact</a></li>
+                                    <li><a href="{{ route('menu.cart') }}">Cart</a></li>
                                     @auth
                                     <li><a href="#">My Account<i class="fi-rs-angle-down"></i></a>
                                         
