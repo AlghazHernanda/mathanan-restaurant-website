1fd0cb76d415e8154ff24a40da08501b90bd6d1f
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-24 15:39:49 +0700

    add logic login

--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -77,52 +77,104 @@
                            
                             <div class="col-lg-6 col-md-12">
 
-                                <form action="/checkout" method="GET">
-                                @csrf
+                                {{-- jika sudah login, tampilkan form yang ini --}}
+                                @auth
+                                    <form action="/checkout" method="GET">
+                                        @csrf
 
-                                <div class="border p-md-4 p-30 border-radius cart-totals">
-                                    <div class="heading_s1 mb-3">
-                                        <h4>Cart Totals</h4>
+                                        <div class="border p-md-4 p-30 border-radius cart-totals">
+                                            <div class="heading_s1 mb-3">
+                                                <h4>Cart Totals</h4>
+                                            </div>
+                                            <div class="table-responsive">
+                                                <table class="table">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td class="cart_total_label">Cart Subtotal</td>
+                                                            <td class="cart_total_amount"><span class="font-lg fw-900 text-brand">${{ Cart::subtotal() }}</span></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="cart_total_label">Tax</td>
+                                                            <td class="cart_total_amount"><span class="font-lg fw-900 text-brand">${{ Cart::tax() }}</span></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="cart_total_label">Name</td>
+                                                            <td class="cart_total_amount"> <i class="ti-gift mr-5"></i> 
+                                                                <input type="text" id="name" name="name" value="{{ Auth::user()->name }}">
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="cart_total_label">Phone Number</td>
+                                                            <td class="cart_total_amount"> <i class="ti-gift mr-5"></i> 
+                                                                <input type="number" id="name" name="phonenumber">
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td class="cart_total_label">Total</td>
+                                                            <td class="cart_total_amount"><strong><span class="font-xl fw-900 text-brand">${{ Cart::total() }}</span></strong></td>
+                                                        </tr>
+
+                                                        <input type="hidden" name="cart" value="{{ Cart::content() }}">
+
+                                                        <input type="hidden" name="total_price" value="{{ Cart::total() }}">
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <button type="submit" class="btn"> <i class="fi-rs-box-alt mr-10"></i> Proceed To CheckOut</button>
+                                        </div>
                                     </div>
-                                    <div class="table-responsive">
-                                        <table class="table">
-                                            <tbody>
-                                                <tr>
-                                                    <td class="cart_total_label">Cart Subtotal</td>
-                                                    <td class="cart_total_amount"><span class="font-lg fw-900 text-brand">${{ Cart::subtotal() }}</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="cart_total_label">Tax</td>
-                                                    <td class="cart_total_amount"><span class="font-lg fw-900 text-brand">${{ Cart::tax() }}</span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="cart_total_label">Name</td>
-                                                    <td class="cart_total_amount"> <i class="ti-gift mr-5"></i> 
-                                                        <input type="text" id="name" name="name">
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="cart_total_label">Phone Number</td>
-                                                    <td class="cart_total_amount"> <i class="ti-gift mr-5"></i> 
-                                                        <input type="number" id="name" name="phonenumber">
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="cart_total_label">Total</td>
-                                                    <td class="cart_total_amount"><strong><span class="font-xl fw-900 text-brand">${{ Cart::total() }}</span></strong></td>
-                                                </tr>
+                                    </form>
 
-                                                <input type="hidden" name="cart" value="{{ Cart::content() }}">
+                                    {{-- jika tidak login, tampilkan form yang ini --}}
+                                    @else
+                                    <form action="/checkout" method="GET">
+                                    @csrf
 
-                                                <input type="hidden" name="total_price" value="{{ Cart::total() }}">
+                                    <div class="border p-md-4 p-30 border-radius cart-totals">
+                                        <div class="heading_s1 mb-3">
+                                            <h4>Cart Totals</h4>
+                                        </div>
+                                        <div class="table-responsive">
+                                            <table class="table">
+                                                <tbody>
+                                                    <tr>
+                                                        <td class="cart_total_label">Cart Subtotal</td>
+                                                        <td class="cart_total_amount"><span class="font-lg fw-900 text-brand">${{ Cart::subtotal() }}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="cart_total_label">Tax</td>
+                                                        <td class="cart_total_amount"><span class="font-lg fw-900 text-brand">${{ Cart::tax() }}</span></td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="cart_total_label">Name</td>
+                                                        <td class="cart_total_amount"> <i class="ti-gift mr-5"></i> 
+                                                            <input type="text" id="name" name="name">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="cart_total_label">Phone Number</td>
+                                                        <td class="cart_total_amount"> <i class="ti-gift mr-5"></i> 
+                                                            <input type="number" id="name" name="phonenumber">
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td class="cart_total_label">Total</td>
+                                                        <td class="cart_total_amount"><strong><span class="font-xl fw-900 text-brand">${{ Cart::total() }}</span></strong></td>
+                                                    </tr>
 
-                                            </tbody>
-                                        </table>
+                                                    <input type="hidden" name="cart" value="{{ Cart::content() }}">
+
+                                                    <input type="hidden" name="total_price" value="{{ Cart::total() }}">
+
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                        <button type="submit" class="btn"> <i class="fi-rs-box-alt mr-10"></i> Proceed To CheckOut</button>
                                     </div>
-                                    <button type="submit" class="btn"> <i class="fi-rs-box-alt mr-10"></i> Proceed To CheckOut</button>
                                 </div>
-                            </div>
-                            </form>
+                                </form>
+                            @endauth
 
                         </div>
                     </div>
