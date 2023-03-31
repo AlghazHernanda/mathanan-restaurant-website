b9477ea996cb50c9393638ffd019ec9da719a8e4
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-03-31 12:27:19 +0700

    cleaning

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -171,33 +171,3 @@
     </main>
 </div>
 
-<div class="modal" id="deleteConfirmation">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-body pb-30 pt-30">
-                <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h4 class="pb-30">Do you want to delete this record!</h4>
-                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Cancel</button>
-                        <button type="button" class="btn btn-danger" onclick="deleteorder()">Delete</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-@push('scripts')
-    <script>
-        function deleteConfirmation(id)
-        {
-            @this.set('order_id',id);
-            $('#deleteConfirmation').modal('show');
-        } 
-        function deleteorder()
-        {
-            @this.call('deleteorder');
-            $('#deleteConfirmation').modal('hide');
-        }
-    </script>
-@endpush
