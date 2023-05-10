d43cb9d4a5727e8c65c305886af7e6c94852038f
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-05-10 19:28:06 +0700

    add migration

--- database/migrations/2023_03_17_085539_create_menus_table.php
+++ database/migrations/2023_03_17_085539_create_menus_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->string('slug')->unique();
             $table->text('description')->nullable();
             $table->decimal('regular_price', 8, 2);
