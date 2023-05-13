137c39af08c664f71bed48367a68dc2e16021be3
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-05-13 22:31:19 +0700

    add

--- app/Http/Livewire/Admin/AdminAddMenuComponent.php
+++ app/Http/Livewire/Admin/AdminAddMenuComponent.php
@@ -45,7 +45,7 @@ class AdminAddMenuComponent extends Component
             'description' => 'required',
             'regular_price' => 'required|numeric',
             'quantity' => 'required|numeric',
-            'image' => 'required|mimes:jpeg,png',
+            'image' => 'required|mimes:jpeg,png,JPEG',
         ]);
 
         $menu = new Menu();
