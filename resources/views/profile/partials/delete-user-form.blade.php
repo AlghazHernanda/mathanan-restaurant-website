82c75c48a59a60c60854f2508648ff97b5a4611d
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-05-23 22:19:14 +0700

    remove delete admin button for testing

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -9,10 +9,16 @@
         </p>
     </header>
 
+    @if (Auth::user()->utype == 'ADM')
+    <br>
+    <br>
+      <strong>tombol delete admin dihilangkan sementara saat blackbox testing</strong>  
+    @else
     <x-danger-button
         x-data=""
         x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
     >{{ __('Delete Account') }}</x-danger-button>
+    @endif
 
     <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
