6cf82f2e400e5b1160adf6ebbe0713d7d357033f
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2023-05-24 00:48:39 +0700

    change header font

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -12,10 +12,10 @@
             <div class="container pt-25">
                 <div class="row">
                     <div class="col-lg-6 align-self-center mb-lg-0 mb-4">
-                        <h6 class="mt-0 mb-15 text-uppercase font-sm text-brand wow fadeIn animated">Mathanan Restaurant</h6>
-                        <h1 class="font-heading mb-40">
+                        <h1 class="mt-0 mb-15 text-uppercase font-heading text-brand wow fadeIn animated">Mathanan Restaurant</h1>
+                        <h3 class="font-heading mb-40">
                            Kami Menyediakan Makanan yang lezat dan bergizi
-                        </h1>
+                        </h3>
                         <p>Mathanan adalah usaha penjualan makanan yang sudah berdiri sejak tahun 2020, Mathanan saat ini sudah mempunyai cabang di Pantai Indah Kapuk (PIK) dan juga dijalankan dirumah daffa madri atha untuk pemesanan di kota Cikarang dan sekitarnya.</p>
                         <p>Mathanan saat ini berjualan dan menerima pesanan secara offline dan online. Untuk pemesanan offline dilakukan didaerah Pantai Indah Kapuk (PIK) dan untuk pemesanan online dilakukan di kota Cikarang. </p>
                     </div>
